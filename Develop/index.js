1ea2f9e5d224026b91e8603563629118bf45c166
Av720 <120581801+Av720@users.noreply.github.com>
2023-03-02 22:44:33 -0700

    added prompt inquirer object

--- Develop/index.js
+++ Develop/index.js
@@ -5,11 +5,80 @@ const fs = require('fs')// fs package
 const generateMarkdown = require('./utils/generateMarkdown'); // generateMarkdown Package 
 const util = require('util'); //util package 
 
-
-
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt = ([
+    {
+        type: 'Input',
+        name: 'title',
+        message: 'What is the current title of your Repository?',
 
+    },
+    {
+        type: '',
+        name: '',
+        message: '',
+
+    },
+    {
+        type: '',
+        name: '',
+        message: '',
+
+    },
+    {
+        type: '',
+        name: '',
+        message: '',
+
+    },
+    {
+        type: '',
+        name: '',
+        message: '',
+
+    },
+    {
+        type: '',
+        name: '',
+        message: '',
+    },
+
+    {
+        type: '',
+        name: '',
+        message: '',
+    },
+
+    {
+        type: '',
+        name: '',
+        message: '',
+    },
+    {
+        type: '',
+        name: '',
+        message: '',
+    },
+    {
+        type: '',
+        name: '',
+        message: '',
+    },
+
+    //     //create a validation if and else for user input 
+    //     validate: nameEntry => {
+    //     if (nameEntry) {
+    //         return true;
+    //     } else {
+    //         console.log('A repository title is required ');
+    //         return false
+    //     }
+    // }
+
+]);
+
+
+console.log(inquirer);
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { }
 
