7e2f22f9647c696aec8a1b595ba0f83f7ca7a419
Av720 <120581801+Av720@users.noreply.github.com>
2023-03-03 23:35:33 -0700

    aded table of contents and created hyperlinks from the user input data

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -27,7 +27,35 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}`;
-}
+  return `# ${data.title}
 
+
+${renderLicenseBadge(data.license)}
+
+## Description 
+${data.description}
+
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+${renderLicenseLink(data.license)}
+-[Test](#test)
+- [Questions](#questions)
+- Contributing(#contributing)
+
+## Installation 
+${data.installation}
+
+## Usage 
+${data.uusage}
+
+## Questions 
+If you havbe any questions please contact me at the following email address: ${data.email}
+
+Find me on GitHub: [${data.username}](Https://github.com/${data.username})
+
+`;
+  
+}
 module.exports = generateMarkdown;
